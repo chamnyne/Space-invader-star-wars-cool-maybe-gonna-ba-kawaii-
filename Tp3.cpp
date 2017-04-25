0e310c3fe3969eb391ea00dfd2881efb36e5e8b6
Kefurx <po.boucher.wrd@gmail.com>
2017-04-25 11:27:53 -0400

    Add files via upload

--- Tp3.cpp
+++ Tp3.cpp
@@ -4,6 +4,7 @@ Programme :			tp3.cpp
 Acteur :            Pierre-Olivier Boucher
 Date de création:	2017/04/25
 Description :		Jeux dans le style d'un Space invader a thème de star wars
+
 /* Directive au pré-processeur
 =============================== */
 #include <iostream>
@@ -17,85 +18,110 @@ using namespace sf;
 class ships
 {
 private:
-	int posX;	// Définit la position horizontal d'un vaiseau 
-	int posY;	// Définit la position verticale d'un vaiseau
+	int _posX;							// Définit la position horizontal d'un vaiseau 
+	int _posY;							// Définit la position verticale d'un vaiseau
 
+	RenderTexture _renderTexture;	// Espace d'un vaiseau
+	
+	Texture _texture;				// Image d'un vaiseau
+
+	Sprite _sprite;					// Sprite d'un vaiseau
+
+public:
+
+	void init(int x, int y,string img)
+	{
+		_renderTexture.create(x, y);
+		_texture.loadFromFile(img);
+		_sprite.setTexture(_texture);
+	}
+
+	void afficher(RenderWindow &window)
+	{
+		_renderTexture.clear();
+		_renderTexture.draw(_sprite);
+		_renderTexture.display();
+	
+		window.draw(_sprite);
+	}
+
+	void setX(int x)
+	{
+		// array largeur fenetre
+		x = _posX;
+	}
+
+	void setY(int y)
+	{
+		// array grandeur fenetre
+		y = _posY;
+	}
+
+	int getX()
+	{
+		return _posX;
+	}
+
+	int getY()
+	{
+		return _posY;
+	}
+
+	void initPosition(int x, int y)
+	{
+		setX(x);
+		setY(y);
+
+		_sprite.setPosition(x, y);
+	}
 };
 
-/*class enemies
-{
-	public vaisseau
-}*/
 
 /* Programme principale
 =========================*/
 int main()
 {
 	// Cération de la fenêtre
-	RenderWindow window(sf::VideoMode(1000, 650), "DEFENDERS OF THE GALAZY");
-
+	RenderWindow window(VideoMode(1000, 800), "DEFENDERS OF THE GALAZY");
+	
 	// Variable
 
 	Event event;				// Evenement de la fenetre			
 
+	ships player;				// Vaiseau du joueur
+	player.init(100, 100, "img/FM4.png");
+	
 	RenderTexture background;	// RenderTexture du background
-	background.create(1000, 650);
-	RenderTexture player;		// RenderTexture du player
-	player.create(100, 100);
-
+	background.create(1000, 800);
+	
 	Texture texBackground;		// Texture du background
-	texBackground.loadFromFile("img/BG2.jpg");
-	Texture texPlayer;			// Texture du joueur -> Going to be in class
-	texPlayer.loadFromFile("img/FM4.png");
+	texBackground.loadFromFile("img/BG2.jpg");	
 
 	Sprite sprtBackground;		// Sprite du background
 	sprtBackground.setTexture(texBackground);
-	Sprite sprtPlayer;			// Sprite du player
-	sprtPlayer.setTexture(texPlayer);
-
-	sprtPlayer.setPosition(500, 535);
 
 	// on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
-	while (window.isOpen())
+	while (window.isOpen()) 
 	{
 		// on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
 		while (window.pollEvent(event))
 		{
-
+			
 			if (event.type == Event::Closed) // évènement "fermeture demandée" : on ferme la fenêtre
 				window.close();
-
-			if (event.type == Event::KeyPressed)
-			{
-				switch (event.key.code)
-				{
-				case Keyboard::Escape:
-					window.close();
-					break;
-				case Keyboard::Left:
-					sprtPlayer.move(-10, 0);
-					break;
-				case Keyboard::Right:
-					sprtPlayer.move(10, 0);
-					break;
-				}
-			}
 		}
-
+		
 		// Efface la fenetre
-		window.clear(Color::Black);
-
+		window.clear(Color::Black); 
+		
 		background.clear();
-		player.clear();
 		background.draw(sprtBackground);
-		player.draw(sprtPlayer);
 		background.display();
-		player.display();
 
 		// Dessine dans la fenêtre
 		window.draw(sprtBackground);
-		window.draw(sprtPlayer);
-
+		player.afficher(window);
+		
 		// Affiche ce qui est dessiné
 		window.display();
 	}
